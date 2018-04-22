543e30a7b163f31381eb44599b69881697528bc1
luckyLocas <1973764398@qq.com>
2018-04-22 21:47:24 +0800

    init

--- vue-music/src/App.vue
+++ vue-music/src/App.vue
@@ -1,23 +1,24 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <m-header />
+    <tab />
   </div>
 </template>
 
-<script>
-export default {
-  name: 'App'
-}
+<script type="text/ecmascript-6">
+  import MHeader from 'components/m-header/m-header'
+  import Tab from 'components/tab/tab'
+  export default {
+    components: {
+      MHeader,
+      Tab
+    }
+  }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+  @import "common/stylus/variable"
+  #app {
+    color: $color-theme
+  }
 </style>
--- vue-music/src/router/index.js
+++ vue-music/src/router/index.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Rank from 'components/rank/rank'
+import Recommend from 'components/recommend/recommend'
+import Search from 'components/search/search'
+import Singer from 'components/singer/singer'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/rank',
+      component: Rank
+    }, {
+      path: '/recommend',
+      component: Recommend
+    }, {
+      path: '/search',
+      component: Search
+    }, {
+      path: '/singer',
+      component: Singer
     }
   ]
 })
